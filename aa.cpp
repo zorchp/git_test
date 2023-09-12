111c502da04bf5d0d1c1621b46520183d2e12a57
zorchp <zorchp@gmail.com>
2023-09-12 16:28:51 +0800

    nihao

--- aa.cpp
+++ aa.cpp
@@ -1,2 +1,9 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
+
+int main(int argc, char *argv[]) {
+    static int xxx;
+    static int yyy = 0;
+    static int zzz = 17;
+    return 0;
+}
